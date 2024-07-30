169c9d51fbb139b7614b48316125274bf498f05f
Gabriel Oliveira de Souza <gabriel@umobi.com.br>
2024-07-30 17:22:47 -0300

    feat: Update API route for optimal flights in api.php

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Api\FlightController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/flights/optimal', [FlightController::class, 'getOptimalFlights']);
+Route::get('/api/flights/optimal', [FlightController::class, 'getOptimalFlights']);
